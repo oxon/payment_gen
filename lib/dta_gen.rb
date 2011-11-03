be9f0c5596b770d5d66924eea5d49f020f3d2a72
Yves Senn <yves.senn@garaio.com>
2011-11-03 17:12:53 +0100

    the transaction number is required

--- lib/dta_gen.rb
+++ lib/dta_gen.rb
@@ -6,10 +6,17 @@ require "dta_gen/payments"
 require 'dta_gen/dta'
 
 module DtaGen
-  def self.create(path)
-    dta = DtaGen::DTA.new
+
+  def self.generate(transaktions_nummer)
+    dta = DtaGen::DTA.new(transaktions_nummer)
     yield dta
+    dta
+  end
+
+  def self.create(transaktions_nummer, path, &block)
+    dta = generate(transaktions_nummer, &block)
     dta.write_file(path)
     dta
   end
+
 end
