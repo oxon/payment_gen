1400c426186ea9fc41b89b9608af387f2f8103ef
Michael Stämpfli <michael.staempfli@garaio.com>
2011-11-07 15:20:09 +0100

    write EZAG files

--- lib/payment_gen/ezag.rb
+++ lib/payment_gen/ezag.rb
@@ -9,8 +9,20 @@ module PaymentGen
       ezag
     end
 
+    def self.create(path, &block)
+      ezag = generate(&block)
+      ezag.write_file(path)
+      ezag
+    end
+
     def initialize
       @records = []
+    end
+
+    def write_file(path)
+      File.open(path,"w") do |file|
+        write_to(file)
+      end
     end
 
     def write_to(io)
--- spec/lib/payment_gen/ezag_spec.rb
+++ spec/lib/payment_gen/ezag_spec.rb
@@ -2,12 +2,21 @@ require 'spec_helper'
 
 describe PaymentGen::EZAG do
 
+  let(:ezag_file_path) { File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'tmp', 'payments.ezag')) }
+
   it "should generate an EZAG object" do
     result = PaymentGen::EZAG.generate do |ezag|
       ezag << EZAGFactory.create_head_record
     end
     result.should be_kind_of(PaymentGen::EZAG)
     result.records.should have(1).item
+  end
+
+  it "should create a file" do
+    PaymentGen::EZAG.create(ezag_file_path) do |ezag|
+      ezag << EZAGFactory.create_domestic_post_account_record
+    end
+    File.exist?(ezag_file_path).should be_true
   end
 
   describe PaymentGen::EZAG, "file records" do
