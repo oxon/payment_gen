d417be434292d6e4885103f7d6617936eb4b4f51
Michael Stämpfli <michael.staempfli@garaio.com>
2011-11-07 15:54:58 +0100

    add a head record to the EZAG file

--- lib/payment_gen/ezag.rb
+++ lib/payment_gen/ezag.rb
@@ -1,22 +1,23 @@
 module PaymentGen
   class EZAG
 
-    attr_reader :records
+    attr_reader :records, :default_attributes
 
-    def self.generate
-      ezag = PaymentGen::EZAG.new
+    def self.generate(attributes = {})
+      ezag = PaymentGen::EZAG.new(attributes)
       yield ezag
       ezag
     end
 
-    def self.create(path, &block)
-      ezag = generate(&block)
+    def self.create(path, attributes = {}, &block)
+      ezag = generate(attributes, &block)
       ezag.write_file(path)
       ezag
     end
 
-    def initialize
+    def initialize(default_attributes = {})
       @records = []
+      @default_attributes = default_attributes
     end
 
     def write_file(path)
@@ -26,11 +27,16 @@ module PaymentGen
     end
 
     def write_to(io)
+      io.puts head_record.to_s
       @records.each{|record| io.puts record.to_s}
     end
 
     def <<(record)
       @records << record
+    end
+
+    def head_record
+      EZAGRecords::HeadRecord.new @default_attributes
     end
 
   end
--- spec/lib/payment_gen/ezag_spec.rb
+++ spec/lib/payment_gen/ezag_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe PaymentGen::EZAG do
 
+  let(:default_attributes) { {:due_date => '2.4.2019', :account_number => '38-384722-8'} }
   let(:ezag_file_path) { File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'tmp', 'payments.ezag')) }
 
   it "should generate an EZAG object" do
@@ -13,7 +14,7 @@ describe PaymentGen::EZAG do
   end
 
   it "should create a file" do
-    PaymentGen::EZAG.create(ezag_file_path) do |ezag|
+    PaymentGen::EZAG.create(ezag_file_path, default_attributes) do |ezag|
       ezag << EZAGFactory.create_domestic_post_account_record
     end
     File.exist?(ezag_file_path).should be_true
@@ -22,13 +23,13 @@ describe PaymentGen::EZAG do
   describe PaymentGen::EZAG, "file records" do
     let(:record1) { EZAGFactory.create_domestic_post_account_record }
     let(:record2) { EZAGFactory.create_domestic_post_account_record }
+    subject { PaymentGen::EZAG.new(default_attributes) }
 
     before :each do
       stream = StringIO.new
-      file = PaymentGen::EZAG.new
-      file << record1
-      file << record2
-      file.write_to stream
+      subject << record1
+      subject << record2
+      subject.write_to stream
       stream.rewind
       @file_records = stream.readlines
     end
@@ -36,6 +37,18 @@ describe PaymentGen::EZAG do
     it "should add the records to the file" do
       @file_records.should include(record1.to_s + "\n")
       @file_records.should include(record2.to_s + "\n")
+    end
+
+    it "should contain a head record" do
+      @file_records.should include(subject.head_record.to_s + "\n")
+    end
+  end
+
+  describe "head record" do
+    subject { PaymentGen::EZAG.new(default_attributes) }
+
+    it "should have a head record" do
+      subject.head_record.should be_kind_of(PaymentGen::EZAGRecords::HeadRecord)
     end
   end
 
