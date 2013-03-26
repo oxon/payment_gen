3021c2036e46b6b4949ac859231457d4aa78bed7
Björn Bur <bbu@oxon.ch>
2013-03-26 16:22:55 +0100

    Domestic post records should be able to handle messages

--- lib/payment_gen/ezag_records/domestic_post_account_record.rb
+++ lib/payment_gen/ezag_records/domestic_post_account_record.rb
@@ -5,8 +5,24 @@ module PaymentGen
         '22'
       end
 
+      def message_1
+        (data[:message_1] || '').ljust(35, ' ')
+      end
+
+      def message_2
+        (data[:message_2] || '').ljust(35, ' ')
+      end
+
+      def message_3
+        (data[:message_3] || '').ljust(35, ' ')
+      end
+
+      def message_4
+        (data[:message_4] || '').ljust(35, ' ')
+      end
+
       def to_s
-        "036#{due_date}000001#{account_number}#{account_number}01#{transaction_type}#{transaction_number}0000000#{main_section}".ljust(700, ' ')
+        ("036#{due_date}000001#{account_number}#{account_number}01#{transaction_type}#{transaction_number}0000000#{main_section}".ljust(402, ' ') + "#{message_1}#{message_2}#{message_3}#{message_4}").ljust(700)
       end
 
       def main_section
